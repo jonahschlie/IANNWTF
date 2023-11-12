a623f8f868eba179a7e93f728b631c40d9f7838d
Christine Arnoldt <christine.arnoldt@ibm.com>
2023-11-12 16:02:12 +0100

    add loss visualization and fix bug with batching in epochs

--- homework1/main.py
+++ homework1/main.py
@@ -6,7 +6,11 @@ from layer import Layer
 from mlp import MLP
 from cce import CCE
 
-digits = load_digits()
+DIGITS = load_digits()
+LOSS_PER_EPOCH = []
+MINIBATCHSIZE = 200
+NUM_EPOCHS = 20
+ACCURACY_PER_EPOCH = []
 
 
 def one_hot_encoding(target_number):
@@ -50,7 +54,10 @@ def shuffle_data(batch_size, tuples_array):
 
         yield input_batch, target_batch
 
-def train(mlp, minibatch_generator, epochs, cce):
+def calc_accuracy(target_label, predicted_label):
+    return np.equal(np.argmax(target_label, axis=-1), np.argmax(predicted_label, axis=-1))
+
+def train(mlp, target_input_tuples, epochs, cce):
     """
     This function trains a multi-layer perceptron (MLP) using a provided minibatch generator and a specified loss function.
 
@@ -59,28 +66,53 @@ def train(mlp, minibatch_generator, epochs, cce):
     :param epochs: The number of training epochs.
     :param cce: The categorical cross-entropy loss function.
     """
-
+    num_samples = len(target_input_tuples)  # Get the total number of samples in the dataset
+    num_minibatches = num_samples // MINIBATCHSIZE  # Calculate the number of minibatches
+    
+    loss_per_batch = dict.fromkeys(list(range(1, num_minibatches+1)))
     for i in range(0, epochs):
+        print(f"Epoch {i}")
+        minibatch_generator = shuffle_data(MINIBATCHSIZE, target_input_tuples)
+        current_minibatch=1
+        truePred = 0
+        falsePred = 0
         for input_batch, target_batch in minibatch_generator:
             output = mlp.forward(input_batch)  # passing the input data through the network
+            
+            falsePred += np.size(calc_accuracy(target_batch, output)) - np.count_nonzero(calc_accuracy(target_batch, output))
+            truePred += np.count_nonzero(calc_accuracy(target_batch, output))
+            
             loss = cce(output, target_batch)  # calculating the loss value
             #print(loss)
+            loss_per_batch[current_minibatch] = loss
             dcce = cce.backward(output, target_batch)
             mlp.backward(dcce)  # updating the networks weights and biases through backpropagation
+            current_minibatch +=1
+        ACCURACY_PER_EPOCH.append(truePred/(truePred+falsePred))
+        LOSS_PER_EPOCH.append(sum(loss_per_batch.values())/len(loss_per_batch))
 
 def main():
     np.random.seed(42)
-    
-    # generating input-target-tuples from training data
-    target_input_tuples = [(2 * (digits.data[i].astype(np.float32) / 16) -
-                            1, one_hot_encoding(digits.target[i])) for i in range(len(digits.data))]
 
-    minibatch_generator_object = shuffle_data(1, target_input_tuples)
+    # generating input-target-tuples from training data
+    target_input_tuples = [(2 * (DIGITS.data[i].astype(np.float32) / 16) -
+                            1, one_hot_encoding(DIGITS.target[i])) for i in range(len(DIGITS.data))]
 
     mlp_object = MLP(2, [64, 10])  # initializing the MLP object
     cce_object = CCE()  # initializing the CCE object
     
-    train(mlp_object, minibatch_generator_object, 1, cce_object)
+    train(mlp_object, target_input_tuples, NUM_EPOCHS, cce_object)
+    
+    plt.plot(list(range(1, NUM_EPOCHS+1)), LOSS_PER_EPOCH)
+    plt.xlabel("Epoch")
+    plt.ylabel("Categorical Cross Entropy Loss")
+    plt.show()
+    
+    #plt.plot(list(range(1, NUM_EPOCHS+1)), ACCURACY_PER_EPOCH)
+    #plt.xlabel("Epoch")
+    #plt.ylabel("Accuracy")
+    #plt.show()
+
 
 
 if __name__ == "__main__":
